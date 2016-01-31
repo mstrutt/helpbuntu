9b054146bd30028bf76025af71673dfa4bf7f9e6
Michael Strutt <michael@mstrutt.co.uk>
2016-01-31 17:11:49 +0000

    typo

--- _content/guides.php
+++ _content/guides.php
@@ -1,7 +1,7 @@
 <?php
 	$subpages = array(0 =>
 		array("url" => "installation", "name" => "Installation"),
-		array("url" => "wireless", "name" => "WG111v3 Wireless")
+		array("url" => "wireless", "name" => "WG111v3 Wireless"),
 		array("url" => "terminal", "name" => "Using Terminal")
 	);
 ?>
